ec566269ed9c58ad3d9311595cd819284d9f0055
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-09-17 23:45:56 +0530

    Feat: Added Basic Functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [emoji, setEmoji] = useState("");
+  const emojiDirectory = {
+    "🏁": "Racing Flag",
+    "🏴‍☠️": "Pirate Flag",
+    "🏳️‍🌈": " Rainbow Flag",
+    "🚩": "Triangular Flag",
+  };
+  function onInputChange(event) {
+    var userInput = event.target.value;
+    var emoji = emojiDirectory[userInput];
+
+    setEmoji(emoji);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Flag Interpretor </h1>
+      <input onChange={onInputChange} placeholder="Enter Emoji" />
+      <div className="displayEmojiText">
+        <h2>meaning: {emoji}</h2>{" "}
+      </div>
     </div>
   );
 }
-
 export default App;
