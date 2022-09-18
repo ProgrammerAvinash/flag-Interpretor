617c710a7743871cbc19dcfa637f385c0cc8ba69
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-09-18 20:35:27 +0530

    Feat : Added onFlagClick Functionality

--- src/App.js
+++ src/App.js
@@ -13,15 +13,41 @@ function App() {
     var userInput = event.target.value;
     var emoji = emojiDirectory[userInput];
 
+    if (emoji === undefined) {
+      emoji = "No data Found ... Please Try With other Flag Emoji";
+    }
     setEmoji(emoji);
   }
 
+  var emojiArray = Object.keys(emojiDirectory);
+  var emojiMapArray = emojiArray.map((item) => {
+    return item;
+  });
+
+  function onEmojiClicked(item) {
+    var emoji = emojiDirectory[item];
+    setEmoji(emoji);
+  }
   return (
     <div className="App">
-      <h1> Flag Interpretor </h1>
+      <h1> -- Flag Interpretor -- </h1>
       <input onChange={onInputChange} placeholder="Enter Emoji" />
       <div className="displayEmojiText">
-        <h2>meaning: {emoji}</h2>{" "}
+        <h2>meaning: {emoji}</h2>
+        <h3>
+          Flags in our Database:
+          {emojiMapArray.map((item) => {
+            return (
+              <span
+                onClick={() => onEmojiClicked(item)}
+                key={item}
+                style={{ padding: "1rem" }}
+              >
+                {item}
+              </span>
+            );
+          })}
+        </h3>
       </div>
     </div>
   );
